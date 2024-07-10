8c687b2ab63abff1ef952443d6285a6022b42395
Isaac Detert <detert7392@students.cptc.edu>
2024-07-09 23:32:20 -0700

    Expands heavily upon the birthday input, and tells the user the program is done after they answer all questions.

--- GettingIntoSourceControl/Program.cs
+++ GettingIntoSourceControl/Program.cs
@@ -1,23 +1,36 @@
-﻿Console.WriteLine("This program will ask for multiple inputs for multiple questions.");
+﻿Console.WriteLine("This program will ask for your input for multiple questions.");
 Console.WriteLine("Please type your name: ");
+Console.WriteLine("");
 
 string name;
 string yesno;
+string age;
 
 name = Console.ReadLine();
 
+Console.WriteLine("");
 Console.WriteLine("Hello " + name + "!");
 Console.WriteLine("Is today your birthday?");
+Console.WriteLine("");
 
 yesno = Console.ReadLine();
 
+Console.WriteLine("");
 if (yesno == "yes")
 {
     Console.WriteLine("Happy Birthday!");
+    Console.WriteLine("How old will you be?");
 } else if (yesno == "no")
 {
     Console.WriteLine("Awww...");
+    Console.WriteLine("Well, how old would you be if it was your birthday?");
 } else
 {
-    Console.WriteLine("I didn't get a yes or no, I am a simple program and thus do not understand your input.");
+    Console.WriteLine("I didn't get a yes or no, I am a simple program and thus do not understand your input, however...");
+    Console.WriteLine("How old would you be if it was your birthday?");
 }
+Console.WriteLine("");
+
+age = Console.ReadLine();
+
+Console.WriteLine("Neat! That is all this program has to ask.");
