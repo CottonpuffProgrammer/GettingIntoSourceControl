d8b6ffa6299c4234d2130033c9ffb39ec812a5ed
Isaac Detert <detert7392@students.cptc.edu>
2024-07-09 23:06:41 -0700

    Added code that asks if it's your birthday or not.

--- GettingIntoSourceControl/Program.cs
+++ GettingIntoSourceControl/Program.cs
@@ -2,7 +2,19 @@
 Console.WriteLine("Please type your name: ");
 
 string name;
+string yesno;
 
 name = Console.ReadLine();
 
 Console.WriteLine("Hello " + name + "!");
+Console.WriteLine("Is today your birthday?");
+
+yesno = Console.ReadLine();
+
+if (yesno == "yes")
+{
+    Console.WriteLine("Happy Birthday!");
+} else if (yesno == "no")
+{
+    Console.WriteLine("Awww...");
+}
